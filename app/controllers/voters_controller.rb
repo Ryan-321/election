d35224ccc26251f2a415f4a698cf9e8393290b4d
Ryan Kahle <jrkahle@hotmail.com>
2015-11-10 15:15:03 -0500

    added validations

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,8 +1,25 @@
 class VotersController < ApplicationController
 
-def index
-  @voters = Voter.all.order(created_at: :desc)
-end
+  def index
+    @voters = Voter.all.order(created_at: :desc)
+  end
+  def show
+  end
+  def new
+  end
+  def created
+  end
+  def edit
+  end
+  def updated
+  end
+  def destroy
+  end
+
+private # am
+  def voter_params
+
+  end
 
 
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,3 @@
 class Voter < ActiveRecord::Base
+    validates :first_name, :last_name, :address, :city, :state, :zip, presence: true
 end
