1718d55790b57908d3db0cefff88b0950582f511
Ryan Kahle <jrkahle@hotmail.com>
2015-11-11 14:58:42 -0500

    modal working

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -2,23 +2,30 @@ class VotersController < ApplicationController
 
   def index
     @voters = Voter.all.order(created_at: :desc)
+    @voter = Voter.new 
   end
   def show
   end
   def new
   end
-  def created
+  def create
+    @voter = Voter.create!(voter_params)
+    redirect_to voters_path
   end
   def edit
+
   end
   def updated
   end
   def destroy
+    @voter = Voter.find(params[:id])
+    @voter.destroy
+    redirect_to voters_path
   end
 
 private # am
   def voter_params
-
+    params.require(:voter).permit(:first_name, :last_name, :address, :city, :state, :zip)
   end
 
 
