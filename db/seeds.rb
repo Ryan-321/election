7adf3966c328cc3a73d7655f7db177a7e59e6b31
Ryan Kahle <jrkahle@hotmail.com>
2015-11-17 11:59:32 -0500

    cleaned up code and modified interface

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,8 @@
 
 Voter.destroy_all
 
-Voter.create(first_name:"John",last_name:"Wall",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
+Voter.create(first_name:"Marcin",last_name:"Gortat",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
+Voter.create(first_name:"Kris",last_name:"Humphries",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
+Voter.create(first_name:"Otto",last_name:"Porter",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
 Voter.create(first_name:"Bradley",last_name:"Beal",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
+Voter.create(first_name:"John",last_name:"Wall",address:"601 F St NW", city:"Washington", state:"DC", zip:2004)
